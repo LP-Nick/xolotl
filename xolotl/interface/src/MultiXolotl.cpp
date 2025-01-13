038b3012121bf46f6c8fee3eeb97496cd54a116f
Nick Militello <nmilitel@vols.utk.edu>
2025-01-13 15:41:11 -0500

    Fixed bug that prevented using matchstep option with multi-instance Xolotl

--- xolotl/interface/src/MultiXolotl.cpp
+++ xolotl/interface/src/MultiXolotl.cpp
@@ -366,6 +366,11 @@ MultiXolotl::updateTemperaturesAndRates(
 void
 MultiXolotl::solveStep()
 {
+	//Skip the first timestep because it doesn't do anything
+	
+	if(currentTime() == 0.0)
+		return;
+
 	// Transfer the temperature to the full network
 	auto subInstanceData = getSubInstanceData();
 	// 0D
